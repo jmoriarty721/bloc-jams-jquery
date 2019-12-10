0298311fcb27bf54b19ff88d65d389feefe76826
J.Moriarty <j.moriarty721@gmail.com>
2019-12-10 15:04:22 -0500

    edits for sliders

--- scripts/player.js
+++ scripts/player.js
@@ -13,15 +13,12 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
-      // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
-      // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
-      // Update our currentlyPlaying and playState properties
+
       this.currentlyPlaying = song;
       this.playState = 'stopped';
       this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
@@ -37,12 +34,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +47,3 @@ class Player {
 }
 
 const player = new Player();
-
